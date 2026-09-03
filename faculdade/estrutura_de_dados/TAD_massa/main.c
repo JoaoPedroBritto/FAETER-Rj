428a1cd4c6c8f40ddc717982ac16d5babb43a0a2
joaoPedro <joaopedrobritto.a@gmail.com>
2026-09-03 18:49:04 -0300

    TAD

---
+++ faculdade/estrutura_de_dados/TAD_massa/main.c
@@ -0,0 +1,24 @@
+// Feito no codeblock's !
+
+/* Exercício 2: TAD Massa
+● Contexto: Em sistemas de logística e nutrição, pesos são divididos em quilogramas e
+gramas para garantir exatidão em operações numéricas inteiras.
+● Enunciado Técnico: Construa a interface (massa.h) e a implementação (massa.c) do tipo
+abstrato Massa. O tipo oculta dois inteiros: quilogramas e gramas.
+● Requisitos do TAD:
+1. mas_cria(kg, g) e mas_libera(m): Criação com ajuste automático de transbordo (1000g =
+1kg) e liberação de memória.
+2. mas_paraString(m): (Obrigatório) Aloca dinamicamente e retorna uma string formatada
+no padrão X,XXXkg (ex: &quot;82,450kg&quot;).
+3. mas_paraGramas(m): Converte a massa total para gramas inteiras.
+4. mas_soma(m1, m2): Retorna uma nova Massa alocada dinamicamente correspondente à
+soma dos dois operandos. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    return 0;
+}
